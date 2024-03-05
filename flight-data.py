2000cd662a8e03442705b2188e6ddce769b8cf60
Sarah Cloughley <smcloughley@gmail.com>
2024-03-04 20:50:27 -0500

    added thread to get/print data every 5 seconds

--- flight-data.py
+++ flight-data.py
@@ -3,19 +3,30 @@
 
 from contextlib import closing
 from urllib.request import urlopen, URLError
-import json
+import json, threading, time
 
 # store Waterloo IP
 url="http://192.168.1.101/skyaware/data/aircraft.json"
 
-# get aircraft data
-with closing(urlopen(url, None, 5.0)) as aircraft_file:
-    aircraft_data = json.load(aircraft_file)
+def print_data():
 
-# print select data for each aircraft
-for a in aircraft_data['aircraft']:
-    flight = a.get('flight')
-    lat = a.get('lat')
-    lon = a.get('lon')
-    if lat and lon:
-        print("Flight number: {flight} Latitude: {lat:.4f} Longitude: {lon:.4f}".format(flight=flight, lat=lat, lon=lon))
+    while 1:
+
+        # get aircraft data
+        with closing(urlopen(url, None, 5.0)) as aircraft_file:
+            aircraft_data = json.load(aircraft_file)
+
+        # print select data for each aircraft
+        for a in aircraft_data['aircraft']:
+            flight = a.get('flight')
+            lat = a.get('lat')
+            lon = a.get('lon')
+            if lat and lon:
+                print("Flight number: {flight} Latitude: {lat:.4f} Longitude: {lon:.4f}".format(flight=flight, lat=lat, lon=lon))
+
+        print('=' * 61)
+        time.sleep(5)
+
+data_loop_thread = threading.Thread(target=print_data)
+data_loop_thread.start()
+
