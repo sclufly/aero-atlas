b1bd3f515366f703943197b56356a3d1e50dd26d
Sarah Cloughley <smcloughley@gmail.com>
2024-04-21 02:01:48 -0400

    finished map styling and started cleaning up site styling

--- client/map.js
+++ client/map.js
@@ -13,8 +13,42 @@ import LineString from 'ol/geom/LineString.js';
 import {Vector as VectorLayer} from 'ol/layer.js';
 import {getVectorContext} from 'ol/render.js';
 import {getWidth} from 'ol/extent.js';
-import {Stroke, Style, Circle as CircleStyle, Fill, Icon} from 'ol/style.js';
+import {Stroke, Style, Circle as CircleStyle, Fill} from 'ol/style.js';
 
+
+// === CONSTANTS ===
+const COLOURS = {
+    bg: '#282828',
+    bg0_h: '#1d2021',
+    dark_red: '#cc241d',
+    red: '#fb4934',
+    bg0: '#282828',
+    bg0_s: '#32302f',
+    dark_green: '#98971a',
+    green: '#b8bb26',
+    bg1: '#3c3836',
+    fg4: '#a89984',
+    dark_yellow: '#d79921',
+    yellow: '#fabd2f',
+    bg2: '#504945',
+    fg3: '#bdae93',
+    dark_blue: '#458588',
+    blue: '#83a598',
+    bg3: '#665c54',
+    fg2: '#d5c4a1',
+    dark_purple: '#b16286',
+    purple: '#d3869b',
+    bg4: '#7c6f64',
+    fg1: '#ebdbb2',
+    dark_aqua: '#689d6a',
+    aqua: '#8ec07c',
+    dark_gray: '#928374',
+    fg0: '#fbf1c7',
+    gray: '#a89984',
+    fg: '#ebdbb2',
+    dark_orange: '#d65d0e',
+    orange: '#fe8019'
+};
 
 // === HEATMAP ===
 
@@ -57,7 +91,7 @@ heatmapLayer.setVisible(false);
 
 // button functionality for changing the current map style
 const mapStyleButton = document.getElementById('map-style-button');
-const mapStyles = ["World_Topo_Map", "World_Imagery", "World_Terrain_Base"];
+const mapStyles = ["World_Terrain_Base", "World_Topo_Map", "World_Imagery"];
 let currentMapStyle = mapStyles[0];
 
 mapStyleButton.addEventListener('click', function () {
@@ -86,7 +120,7 @@ heatmapButton.addEventListener('click', function () {
 
 const view = new View({
     center: [-8800000, 5400000],
-    zoom: 7
+    zoom: 4
 });
 
 // map object
@@ -102,71 +136,55 @@ const map = new Map({
 
 const oriDesStyle = new Style({
     stroke: new Stroke({
-        color: '#ffffff',
-        width: 4,
+        color: COLOURS.gray,
+        width: 5,
+        lineDash: [5, 7],
     }),
 });
 
 const oriDesFinishedStyle = new Style({
     stroke: new Stroke({
-        color: '#979ea1',
+        color: COLOURS.dark_gray,
         width: 4,
+        lineDash: [5, 7],
     }),
 });
 
 const interStyle = new Style({
     stroke: new Stroke({
-        color: '#f26716',
-        width: 4,
+        color: COLOURS.yellow,
+        width: 5,
+        lineDash: [5, 7],
     }),
 });
 
 const interFinishedStyle = new Style({
     stroke: new Stroke({
-        color: '#979ea1',
+        color: COLOURS.yellow,
         width: 4,
+        lineDash: [5, 7],
     }),
 });
 
-const airportStyle = new Style({
-    image: new Icon({
-        anchor: [0.5, 1],
-        anchorXUnits: 'fraction',
-        anchorYUnits: 'fraction',
-        src: '/airport-tower.png',
-        scale: 0.07,
-      }),
+const oriAirportStyle = new Style({
+    image: new CircleStyle({
+        radius: 6,
+        fill: new Fill({color: COLOURS.green}),
+        stroke: new Stroke({
+            color: COLOURS.dark_gray, width: 3
+        })
+    })
 });
 
-// create airport points layer
-const airportsLayer = new VectorLayer({
-    source: new Vector(),
-    style: airportStyle
+const desAirportStyle = new Style({
+    image: new CircleStyle({
+        radius: 6,
+        fill: new Fill({color: COLOURS.red}),
+        stroke: new Stroke({
+            color: COLOURS.dark_gray, width: 3
+        })
+    })
 });
-
-// Iterate over the JSON object to create point features
-flightsData.forEach(function(flight) {
-    const ori = flight.ori;
-    const des = flight.des;
-
-    // create origin point
-    const oriPoint = new Point(fromLonLat([ori[1], ori[0]]));
-    const oriFeature = new Feature({
-        geometry: oriPoint,
-        name: ori[2]
-    });
-    airportsLayer.getSource().addFeature(oriFeature);
-
-    // create destination point
-    const desPoint = new Point(fromLonLat([des[1], des[0]]));
-    const desFeature = new Feature({
-        geometry: desPoint,
-        name: des[2]
-    });
-    airportsLayer.getSource().addFeature(desFeature);
-
-});
-
 
 // create popup for airports
 const popupOverlay = new Overlay({
@@ -208,9 +226,20 @@ const flightsSource = new Vector({
             const ori = flight.ori;
             const des = flight.des;
             const inter = flight.inter || [];
+            let crossesDateLine = false;
+            let dateLineCrossIndex = 0;
             
             let allPoints = [ori, ...inter, des];
             let allGeometries = [];
+
+            // create origin airport point
+            const oriPoint = new Point(fromLonLat([ori[1], ori[0]]));
+            const oriFeature = new Feature({
+                geometry: oriPoint,
+                name: ori[2],
+                finished: true,
+                airport: "origin",
+            });
 
             // Iterate over each pair of consecutive points
             for (let j = 0; j < allPoints.length - 1; j++) {
@@ -225,6 +254,12 @@ const flightsSource = new Vector({
 
                 // Generate the arc line
                 const arcLine = arcGenerator.Arc(100, {offset: 10});
+                
+                // deal with dateline cross
+                if (arcLine.geometries.length > 1) {
+                    crossesDateLine = true;
+                    dateLineCrossIndex = j || 1;
+                }
 
                 // Add arc coordinates to the concatenated points array
                 allGeometries = allGeometries.concat(arcLine.geometries);
@@ -232,11 +267,18 @@ const flightsSource = new Vector({
 
             // paths which cross the -180°/+180° meridian are split
             // into two sections which will be animated sequentially
-            const features = [];
+            const features = [oriFeature];
             allGeometries.map(function (geometry, i) {
+
                 const line = new LineString(geometry.coords);
                 line.transform('EPSG:4326', 'EPSG:3857');
-                const isInter = (i !== 0 && i !== allGeometries.length - 1);
+                let isInter = (i !== 0 && i !== allGeometries.length - 1);
+                const isLast = (i === allGeometries.length - 1);
+
+                // check for dateline cross
+                if (dateLineCrossIndex === i) {
+                    isInter = false;
+                }
 
                 features.push(
                     new Feature({
@@ -244,9 +286,21 @@ const flightsSource = new Vector({
                         finished: false,
                         segmentCount: allGeometries.length,
                         isInter: isInter, 
+                        isLast: isLast,
                     }),
                 );
             });
+
+            // create destination airport point
+            const desPoint = new Point(fromLonLat([des[1], des[0]]));
+            const desFeature = new Feature({
+                geometry: desPoint,
+                name: des[2],
+                finished: false,
+                airport: "destination",
+            });
+            features.push(desFeature);
+
             // add the features with a delay so that the animation
             // for all features does not start at the same time
             addLater(features, i * 500);
@@ -258,15 +312,23 @@ const flightsSource = new Vector({
 const flightsLayer = new VectorLayer({
     source: flightsSource,
     style: function (feature) {
+
         // if the animation is still active for a feature, do not
         // render the feature with the layer style
         if (feature.get('finished')) {
+
+             // if it's a point, use the airport style
+            if (feature.getGeometry().getType() === 'Point') {
+                return (feature.get('airport') === 'origin') ? oriAirportStyle : desAirportStyle;
+            }
+
             if (feature.get('isInter')) {
                 return interFinishedStyle;
             } else {
                 return oriDesFinishedStyle;
             }
         }
+
         return null;
     },
 });
@@ -276,6 +338,9 @@ function animateFlights(event) {
     const vectorContext = getVectorContext(event);
     const frameState = event.frameState;
     const features = flightsSource.getFeatures();
+    features.sort(function(first, second) {
+        return first.ol_uid - second.ol_uid;
+    });
 
     for (let i = 0; i < features.length; i++) {
 
@@ -299,6 +364,10 @@ function animateFlights(event) {
         
                 if (elapsedPoints >= coords.length) {
                     feature.set('finished', true);
+                    // if this is a last segment, also set the next feature (destination airport) to be finished
+                    if (feature.get('isLast')) {
+                        features[i + 1].set('finished', true);
+                    }
                 }
         
                 const maxIndex = Math.min(elapsedPoints, coords.length);
@@ -325,18 +394,16 @@ function animateFlights(event) {
 function addLater(features, timeout) {
     window.setTimeout(function () {
         let start = Date.now();
-
         features.forEach(function (feature) {
             feature.set('start', start);
-            flightsSource.addFeature(feature);
             const segmentSum = feature.getGeometry().getCoordinates().length - 1;
-            const duration = segmentSum / (pointsPerMs * features.length);
+            const duration = segmentSum / (pointsPerMs * (features.length - 2));
             start += duration;
         });
+        flightsSource.addFeatures(features);
     }, timeout);
 }
 
 // add dynamic layers to the map
 map.addLayer(flightsLayer);
-map.addLayer(airportsLayer);
 map.addOverlay(popupOverlay);
